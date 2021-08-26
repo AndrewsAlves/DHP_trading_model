dc9db525ce6367060216491713d9c121f8aff849
Andyalves <andrewsalvistrading@gmail.com>
2021-08-26 14:19:04 +0530

    getting local data

--- DHPTrading.py
+++ DHPTrading.py
@@ -10,4 +10,15 @@ Created on Wed Aug 25 15:15:17 2021
 @author: AndyAlves
 """
 
+import pandas as pd
+import datetime as dt
 
+
+def get_banknifty_data(local = True) : 
+    if local == True:
+        data = pd.read_csv("D:\Qbatomic\Datas\TimeSeries\Banknifty\Intraday_1_Min_Data\intraday_1m_data_after_consolidation\BNF\\bnf_2016-2020_intraday_1m.txt")
+        data.drop(['useless1','useless2'], axis = 1)
+        return data
+        
+        
+bnf_data = get_banknifty_data()
